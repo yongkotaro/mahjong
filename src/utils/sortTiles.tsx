4b2c4040c4a7a0c0a4e36c346de27ebb088ba758
yongkotaro <yongkotaro@gmail.com>
2024-10-10 19:44:42 +0800

    Add google search console verification

--- src/utils/sortTiles.tsx
+++ src/utils/sortTiles.tsx
@@ -1,6 +1,7 @@
 import { extractImageInfo } from './parseTiles';
 
 const suitsOrder = ['pin', 'bamboo', 'man'];
+const honorsOrder = ['dong', 'nan', 'xi', 'bei', 'hong', 'qing', 'baiban'];
 
 export const sortImages = (filenames: string[]): string[] => {
   const imageInfos = filenames.map(extractImageInfo);
@@ -8,19 +9,19 @@ export const sortImages = (filenames: string[]): string[] => {
   imageInfos.sort((a, b) => {
     if (a.isSpecial && b.isSpecial) {
       // Keep the order of special tiles the same
-      return filenames.indexOf(a.src) - filenames.indexOf(b.src);
+      return honorsOrder.indexOf(a.suit) - honorsOrder.indexOf(b.suit);
     } else if (a.isSpecial) {
       return 1; // Move special tiles to the end
     } else if (b.isSpecial) {
       return -1; // Move special tiles to the end
     }
 
-    const suitComparison = suitsOrder.indexOf(a.suit!) - suitsOrder.indexOf(b.suit!);
+    const suitComparison = suitsOrder.indexOf(a.suit) - suitsOrder.indexOf(b.suit);
     if (suitComparison !== 0) {
       return suitComparison;
     }
 
-    return a.number! - b.number!;
+    return a.number - b.number;
   });
 
   // Extract and return sorted filenames
