e4e4212aaaacfe3924e5c58a45e29f3ea3dc4e30
yongkotaro <yongkotaro@gmail.com>
2025-07-07 15:32:57 +0800

    Migrate from CRA to Vite

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
